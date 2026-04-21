ac1310258055b095659eb578b88c530b3c4ca0b5
otisdoan <ledoanhieu12a6@gmail.com>
2026-04-22 02:29:51 +0700

    initialize Spring Boot project with Maven

---
+++ src/main/java/com/example/cellphones_spring/CellphonesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cellphones_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellphonesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellphonesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cellphones_spring/CellphonesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cellphones_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellphonesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
